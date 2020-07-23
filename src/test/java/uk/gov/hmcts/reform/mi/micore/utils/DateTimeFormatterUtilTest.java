29590abbc0e12e94bb214e1bd727a7855cc81b7f
qzhou <qiang.zhou@hmcts.net>
2020-07-23 15:50:17 +0100

    Fix cs and pmd

--- src/test/java/uk/gov/hmcts/reform/mi/micore/utils/DateTimeFormatterUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/mi/micore/utils/DateTimeFormatterUtilTest.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.mi.micore.utils;
 
 import org.hamcrest.object.HasEqualValues;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,8 +17,8 @@ public class DateTimeFormatterUtilTest {
     @ParameterizedTest
     @MethodSource("argumentsProvider")
     void testGetValidDateFormatter(String input, DateTimeFormatter expected, String dataSample) {
-        assertThat( "Expected date time for " + input, expected.parse( dataSample),
-            new HasEqualValues(DateTimeFormatterUtil.getDataFormat(input).parse( dataSample)));
+        assertThat("Expected date time for " + input, expected.parse(dataSample),
+            new HasEqualValues(DateTimeFormatterUtil.getDataFormat(input).parse(dataSample)));
     }
 
     @Test
@@ -26,6 +26,7 @@ public class DateTimeFormatterUtilTest {
         assertNull(DateTimeFormatterUtil.getDataFormat(null), "Expected null");
     }
 
+    @SuppressWarnings("PMD.UnusedPrivateMethod")
     private static Stream<Arguments> argumentsProvider() {
         return Stream.of(
             Arguments.of("ISO_DATE_TIME", DateTimeFormatter.ISO_DATE_TIME, "2011-12-03T10:15:30+01:00[Europe/Paris]"),
