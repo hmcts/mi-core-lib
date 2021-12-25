fe45aa8e5e4b1edacce29bc09fdbe357960e45f3
Tony Chow <tony.chow@hmcts.net>
2021-12-25 22:57:39 +0000

    Disable storage autoconfig

--- src/main/java/uk/gov/hmcts/reform/mi/micore/MiCoreLibConfigurationContext.java
+++ src/main/java/uk/gov/hmcts/reform/mi/micore/MiCoreLibConfigurationContext.java
@@ -1,10 +1,13 @@
 package uk.gov.hmcts.reform.mi.micore;
 
+import com.azure.spring.autoconfigure.storage.StorageAutoConfiguration;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 /**
  * Spring Boot configuration setup.
  */
 @SpringBootConfiguration
+@EnableAutoConfiguration(exclude = StorageAutoConfiguration.class)
 public class MiCoreLibConfigurationContext {
 }
