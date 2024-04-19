481dab7e7a0ea843e707916c77f5ec379acead50
Tony Chow <36265570+tchow8@users.noreply.github.com>
2024-04-19 10:39:18 +0100

    Update scripts for workload identity (#177)
    * Update runExtraScripts.ps1 with workload identity
    
    * Update deploySqlPackage.ps1 with workload identity auth
    
    * Update deploySqlPackage.sh with workload identity auth

--- pipeline-steps/scripts/deploySqlPackage.ps1
+++ pipeline-steps/scripts/deploySqlPackage.ps1
@@ -44,12 +44,28 @@ Function Get-Variables {
 }
 
 Function Set-Managed-Identity-Auth {
-    $response = Invoke-WebRequest `
-         -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fdatabase.windows.net'`
-         -Method GET`
-         -Headers @{Metadata="true"}
+    try {
+        $response = Invoke-WebRequest `
+             -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fdatabase.windows.net'`
+             -Method GET`
+             -Headers @{Metadata="true"}
+        
+        $access_token = ($response.Content | ConvertFrom-Json).access_token
+    } catch {
+        $identity_token = Get-Content -Path $Env:AZURE_FEDERATED_TOKEN_FILE -Raw
+        
+        $formFields = @{grant_type="client_credentials";client_id="$Env:AZURE_CLIENT_ID";scope="https://database.windows.net/.default";client_assertion_type="urn:ietf:params:oauth:client-assertion-type:jwt-bearer";client_assertion="$identity_token"}
+        
+        $workload_response = Invoke-WebRequest `
+            -Uri "$Env:AZURE_AUTHORITY_HOST/$Env:AZURE_TENANT_ID/oauth2/v2.0/token" `
+            -Method POST `
+            -Body $formFields `
+            -ContentType "application/x-www-form-urlencoded"
+        
+        $access_token = ($workload_response.Content | ConvertFrom-Json).access_token
+    }
 
-    ($response.Content | ConvertFrom-Json).access_token
+    return $access_token
 }
 
 $Command='sqlpackage'
--- pipeline-steps/scripts/deploySqlPackage.sh
+++ pipeline-steps/scripts/deploySqlPackage.sh
@@ -16,6 +16,22 @@ function setManagedIdentityAccessToken() {
     if [ -z "$1" ]
     then
       access_token=$(curl -X GET "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fdatabase.windows.net" -H "Metadata: true" | jq -r '.access_token')
+
+      # If access_token is empty then managed identity has been migrated to workload identity.
+      if [ -z "$access_token" ]
+      then
+          IDENTITY_TOKEN=$(cat $AZURE_FEDERATED_TOKEN_FILE)
+    
+          federated_token=$(curl -s --location --request POST "$AZURE_AUTHORITY_HOST/$AZURE_TENANT_ID/oauth2/v2.0/token" \
+            --form 'grant_type="client_credentials"' \
+            --form 'client_id="'$AZURE_CLIENT_ID'"' \
+            --form 'scope="https://database.windows.net/.default"' \
+            --form 'client_assertion_type="urn:ietf:params:oauth:client-assertion-type:jwt-bearer"' \
+            --form 'client_assertion="'$IDENTITY_TOKEN'"')
+    
+          access_token=$(echo $federated_token | jq -r '.access_token')
+      fi
+      
       echo "/AccessToken:$access_token"
     else
         echo ""
